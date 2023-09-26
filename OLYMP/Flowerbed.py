23567abca71568ba36f7d784ce3dec684408e1ac
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2023-09-26 23:34:29 +0600

    Update Flowerbed.py

--- OLYMP/Flowerbed.py
+++ OLYMP/Flowerbed.py
@@ -1,8 +1,11 @@
 print("Введите число")
 k = int(input())
+l = 7
+m = 10
+n = 5
 answer = 0
 
 for i in range(k):
-    answer += 44 + 20 * i
-    
+    answer += 2 * (l + m + n) + 2 * m * i
+
 print("Если грядок " + str(k) + ", то расстояние = " + str(answer))
