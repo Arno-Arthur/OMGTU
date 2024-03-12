a1468f9670bcaa73a79fe60c1d128bde7b841ed7
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-03-12 02:42:54 +0000

    Create Floyd.py

---
+++ DIS_MATH/Floyd.py
@@ -0,0 +1,29 @@
+def floyd_algorithm(graph):
+    n = len(graph)
+    dist = [[float('inf')] * n for _ in range(n)]
+
+    for i in range(n):
+        dist[i][i] = 0
+
+    for u in range(n):
+        for v in range(n):
+            dist[u][v] = graph[u][v]
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+    return dist
+
+
+graph = [
+            [0, 5, float('inf'), 10],
+            [float('inf'), 0, 3, float('inf')],
+            [float('inf'), float('inf'), 0, 1],
+            [float('inf'), float('inf'), float('inf'), 0]
+        ]
+
+result = floyd_algorithm(graph)
+for row in result:
+    print(row)
