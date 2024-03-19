91c3a9ac42c88500e3e3f9b6d604c7bde922664d
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-03-19 21:44:52 +0600

    Create HW_String_2.cs

---
+++ Practicum/HW_String_2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "The time is 12:30";
+        var regex = new Regex(@"\b(?:[01][0-9]|2[0-3]):[0-5][0-9]\b");
+        var matches = regex.Matches(input).Cast<Match>().Select(x => x.Value);
+        foreach (var match in matches)
+        {
+            Console.Write($"Найденное время: {match}");
+        }
+    }
+}
