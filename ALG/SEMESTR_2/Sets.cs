5d6e81bce8b4aef20463966b64692a0e97b4101a
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-03-19 17:51:19 +0600

    Update Sets.cs

--- ALG/SEMESTR_2/Sets.cs
+++ ALG/SEMESTR_2/Sets.cs
@@ -1,46 +1,57 @@
 using System;
+using System.Collections.Immutable;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string[] soft = { "Microsoft", "Google", "Apple", "AMD", "Xiaomi" };
-        string[] hard = { "Apple", "IBM", "Samsung", "Toshiba" };
-        string[] company = { "Apple", "IBM" };
+        /*SortedSet используется вместо HashSet для удобства перечисления элементов множества*/
+        SortedSet<int> numbers1 = new SortedSet<int>();
+        SortedSet<int> numbers2 = new SortedSet<int>();
+        SortedSet<int> numbers3 = new SortedSet<int>();
 
-        Console.WriteLine("Пересечение: ");
-        var cross = soft.Intersect(hard).Intersect(company);
-        foreach (string s in cross)
+        for (int i = 0; i < 13; i++)
         {
-            Console.WriteLine("\t" + s);
+            numbers1.Add(i * 2);
+            numbers2.Add(i * 3);
+            numbers3.Add(i * 4);
         }
 
-        Console.WriteLine("\n" + "Объединение: ");
-        var unity = soft.Union(hard).Union(company);       
-        foreach (string s in unity)
-        {
-            Console.WriteLine("\t" + s);
-        }
+        PrintSet(numbers1, "1 множество: ");
+        PrintSet(numbers2, "2 множество: ");
+        PrintSet(numbers3, "3 множество: ");
 
-        Console.WriteLine("\n" + "Дополнение 1: ");
-        var soft_add = unity.Except(soft);
-        foreach (string s in soft_add)
-        {
-            Console.WriteLine("\t" + s);
-        }
+        SortedSet<int> universalSet = new SortedSet<int>();
+        universalSet.UnionWith(numbers1);
+        universalSet.UnionWith(numbers2);
+        universalSet.UnionWith(numbers3);
+        PrintSet(universalSet, "Объединение: ");
 
-        Console.WriteLine("\n" + "Дополнение 2: ");
-        var hard_add = unity.Except(hard);
-        foreach (string s in hard_add)
-        {
-            Console.WriteLine("\t" + s);
-        }
+        SortedSet<int> complement1 = new SortedSet<int>(universalSet);
+        complement1.ExceptWith(numbers1);
 
-        Console.WriteLine("\n" + "Дополнение 3: ");
-        var company_add = unity.Except(company);
-        foreach (string s in company_add)
-        {
-            Console.WriteLine("\t" + s);
-        }
+        SortedSet<int> complement2 = new SortedSet<int>(universalSet);
+        complement2.ExceptWith(numbers2);
+
+        SortedSet<int> complement3 = new SortedSet<int>(universalSet);
+        complement3.ExceptWith(numbers3);
+
+
+        universalSet.IntersectWith(numbers1);
+        universalSet.IntersectWith(numbers2);
+        universalSet.IntersectWith(numbers3);
+        PrintSet(universalSet, "Пересечение: ");
+
+        PrintSet(complement1, "Дополнение 1: ");
+        PrintSet(complement2, "Дополнение 2: ");
+        PrintSet(complement3, "Дополнение 3: ");
+
+    }
+    static void PrintSet(SortedSet<int> set, string s)
+    {
+        Console.Write(s);
+        foreach (int num in set)
+            Console.Write(num + " ");
+        Console.WriteLine("\n");
     }
 }
