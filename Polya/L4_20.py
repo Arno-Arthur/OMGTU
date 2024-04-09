b63153854d5a34d6096576e91c08dd4285769e88
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-04-09 08:38:19 +0500

    Update L4_20.py

--- Polya/L4_20.py
+++ Polya/L4_20.py
@@ -8,5 +8,11 @@ for i in range(size):
 
 print(f'Ваш массив: {numbers}')
 
-maximum = max(numbers)
+maximum = max(numbers) #Функция-поиск максимального элемента в массиве
+
 print(f'Максимальный элемент: {maximum}\nЕго индекс: {numbers.index(maximum)}\nЕго порядковый номер: {numbers.index(maximum)+1}')
+
+#Разница между индексом и порядковым номером:
+#Индекс от 0 до N, порядковыый номер от 1 до N+1
+#В данном случае индекс от 0 до 19, порядковый номер от 1 до 20
+#numbers.index(maximum) - индекс максимального числа в массиве numbers
