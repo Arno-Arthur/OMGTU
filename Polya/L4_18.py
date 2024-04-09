fce0b450b26cada71f1a59f3aa68226303cb0132
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-04-09 08:08:33 +0500

    Update L4_18.py

--- Polya/L4_18.py
+++ Polya/L4_18.py
@@ -2,7 +2,7 @@ size = int(input("Размер массива (целое число): "))
 numbers = [0] * size
 sum = 0
 
-for i in range(0, size):
+for i in range(size):
     numbers[i] = float(input(f'Элемент массива {i+1}: ' ))
     sum += numbers[i]
 
