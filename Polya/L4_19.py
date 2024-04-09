53f7e55e911c59bdcbaa8371524f15853f11db7a
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-04-09 08:35:25 +0500

    Update L4_19.py

--- Polya/L4_19.py
+++ Polya/L4_19.py
@@ -1,9 +1,9 @@
-from random import random, randint
+from random import randint #Импорт функции randint
 
 size = 20
 numbers = [0] * size
 
 for i in range(size):
-    numbers[i] = randint(-100,100)
+    numbers[i] = randint(-100,100) #Элемент i массива = рандомное число в отрезке от -100 до 100
 
 print(f'Ваш массив: {numbers}')
