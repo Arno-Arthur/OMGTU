04bf7c4d353e6ebb93e8587a71d076d763fd6a66
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-04-09 08:32:29 +0500

    Update L4_17.py

--- Polya/L4_17.py
+++ Polya/L4_17.py
@@ -1,7 +1,7 @@
-size = int(input("Размер массива (целое число): "))
-numbers = [0] * size
+size = int(input("Размер массива (целое число): ")) #Думаю и так понятно
+numbers = [0] * size #Заполняем массив numbers нулями - в количестве size 
 
-for i in range(size):
-    numbers[i] = float(input(f'Элемент массива {i+1}: ' ))
+for i in range(size): #Для каждого i в диапозоне от 0 до size выполняем
+    numbers[i] = float(input(f'Элемент массива {i+1}: ' )) #Элемент i массива numbers вводит пользователь
 
-print(f'Ваш массив: {numbers}')
+print(f'Ваш массив: {numbers}') #Вывод массива 
