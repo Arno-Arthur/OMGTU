0aeee1b74047f5fe64510ff96f6acf2381933a39
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-04-09 08:33:46 +0500

    Update L4_18.py

--- Polya/L4_18.py
+++ Polya/L4_18.py
@@ -1,9 +1,9 @@
 size = int(input("Размер массива (целое число): "))
 numbers = [0] * size
-sum = 0
+sum = 0 #Переменная-сумма
 
 for i in range(size):
     numbers[i] = float(input(f'Элемент массива {i+1}: ' ))
-    sum += numbers[i]
+    sum += numbers[i] #Увеличиваем сумму на каждый элемент массива 
 
 print(f'Ваш массив: {numbers}', f'\nCумма элементов массива: {sum}')
