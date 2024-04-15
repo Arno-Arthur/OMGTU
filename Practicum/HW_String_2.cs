59f9f62c7f64280b808ab52b0f36ac883bd411e4
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-04-15 11:53:25 +0600

    Update HW_String_2.cs

--- Practicum/HW_String_2.cs
+++ Practicum/HW_String_2.cs
@@ -9,9 +9,6 @@ class Program
         string input = "The time is 12:30";
         var regex = new Regex(@"\b(?:[01][0-9]|2[0-3]):[0-5][0-9]\b");
         var matches = regex.Matches(input).Cast<Match>().Select(x => x.Value);
-        foreach (var match in matches)
-        {
-            Console.Write($"Найденное время: {match}");
-        }
+        foreach (var match in matches) Console.Write($"Найденное время: {match}");
     }
 }
