9cfaa1598744d310edb0584d6d83616c11ec2739
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-04-25 21:59:08 +0600

    Update File_3.cs

--- ALG/SEMESTR_2/File_3.cs
+++ ALG/SEMESTR_2/File_3.cs
@@ -1,13 +1,37 @@
-string[] numbers = File.ReadAllLines("900.txt");
-int oddProductPairsCount = 0;
-for (int i = 0; i < numbers.Length; i++)
+class Program
 {
-    for (int j = i + 1; j < numbers.Length; j++)
+    static void Main()
     {
-        int number1 = int.Parse(numbers[i]);
-        int number2 = int.Parse(numbers[j]);
+        string path = "C:\\Users\\Admin\\source\\repos\\ConsoleApp\\ConsoleApp\\TextFile1.txt";
+        
+        FileInfo fileInfo = new FileInfo(path);
 
-        if ((number1 * number2) % 2 != 0) oddProductPairsCount++;
+        if (fileInfo.Exists)
+        {
+            Console.WriteLine($"Имя файла: {fileInfo.Name}");
+            Console.WriteLine($"Время создания: {fileInfo.CreationTime}");
+            Console.WriteLine($"Размер: {fileInfo.Length}");
+        }
+
+        string[] numbers = File.ReadAllLines(path);
+        int oddProductPairsCount = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                int number1 = int.Parse(numbers[i]);
+                int number2 = int.Parse(numbers[j]);
+
+                if ((number1 * number2) % 2 != 0) oddProductPairsCount++;
+            }
+        }
+        Console.WriteLine($"\nКоличество пар элементов, произведение которых нечетное: {oddProductPairsCount}");
+
+        /* Файл: 
+         * 2
+         * 3
+         * 5
+         * 7
+        */
     }
 }
-Console.WriteLine($"Количество пар элементов, произведение которых нечетное: {oddProductPairsCount}");
