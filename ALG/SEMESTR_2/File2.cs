9258a495ebe2cb9c2630e2ea09baab1e8b4dc681
Arno-Arthur <101925121+Arno-Arthur@users.noreply.github.com>
2024-05-07 05:04:53 +0100

    Update File2.cs

--- ALG/SEMESTR_2/File2.cs
+++ ALG/SEMESTR_2/File2.cs
@@ -1,9 +1,10 @@
-// Входной файл:
-// aaaaa
-// abcaaa
-// aa
-// aaa
-// ab
+//Входной файл:
+//aa aaa
+//ab c aaa
+//a a
+//a a a
+//ab
+// 
 // a
 // Результат: 'a'
 class Program
@@ -11,7 +12,7 @@ class Program
     static void Main()
     {
         string filename = "C:\\Users\\User\\Desktop\\txt\\ConsoleApplication2\\ConsoleApplication2\\1.txt"; 
-        Console.WriteLine($"Строка с наименьшей подпоследовательностью 'a': {FindShortestASequence(filename)}"); 
+        Console.WriteLine($"Строка с наименьшей подпоследовательностью 'a': {FindShortestASequence(filename)}");
     }
 
     static string FindShortestASequence(string filename)
@@ -21,9 +22,13 @@ class Program
 
         foreach (var line in File.ReadLines(filename))
         {
-            var sequences = line.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries)
+            var sequences = line.Replace(" ", "").Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries)
                                 .Where(seq => seq.All(c => c == 'a'));
+            
+            if (string.IsNullOrWhiteSpace(line) || !sequences.Any()) continue;
 
+
+            
             foreach (var seq in sequences)
             {
                 if (seq.Length < shortestLength)
@@ -33,7 +38,7 @@ class Program
                 }
             }
         }
-      
-        return lineWithShortestASequence;
+
+        return string.IsNullOrEmpty(lineWithShortestASequence) ? "Нет подходящих строк" : lineWithShortestASequence;
     }
 }
